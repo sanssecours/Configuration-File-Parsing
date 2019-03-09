74ecb0be0f729baad10dee23b81b057d60c8b253
René Schwaiger <sanssecours@me.com>
2019-03-09 12:50:46 +0100

    Scripts: Fix matching in error message script

--- Scripts/error.fish
+++ Scripts/error.fish
@@ -14,7 +14,7 @@ for file in $files
         printf '\n🔌 %s\n\n' "$plugin"
         kdb mount config.yaml user/tests/yaml "$plugin"
         cat "$path/$file" >(kdb file user/tests/yaml)
-        kdb ls user/tests/yaml 2>&1 | grep 'Reason: '
+        kdb ls user/tests/yaml 2>&1 | pcre2grep -M 'Reason: ((?!Ingroup)(\n|.))*'
         reset_kdb >/dev/null
     end
 end
